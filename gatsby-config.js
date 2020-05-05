29a91532cbc0195f0410a8bd05945a281c2abf23
Kyle Gill <kylerobertgill@gmail.com>
2020-05-05 15:14:55 -0600

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Analytics Sandbox`,
+    description: `Site with plugins for testing analytics, event tracking, and experiments`,
+    author: `@gill_kyle`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
