a8d4b08bc9cc5abc893afc17adccdb34d9313b60
Kyle Gill <kylerobertgill@gmail.com>
2020-05-11 13:03:20 -0600

    add 1-3 routes for lookup table testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,8 +42,5 @@ module.exports = {
         exclude: ["*"],
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
---
+++ src/pages/two.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Page Two</h1>
+    <p>This is a page to test lookup tables based on page path.</p>
+  </Layout>
+)
+
+export default IndexPage
